0c5e0fe78df3fcedccb5f7130a7fdea98a86b4ec
Md. Abdul Ahad Chowdhury <maacpiash@outlook.com>
2017-04-29 02:31:58 +0600

    Started working with MySQL.

--- CSE327_Assignment01/Registration.cs
+++ CSE327_Assignment01/Registration.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace CSE327_Assignment01
 {
     public class Registration
     {
+        // create table courses (id char(8) not null primary key, title char(50), credit int, tpc int;
+        // CREATE TABLE courses (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, state CHAR(25), population INT(9));
+        /* insert into courses (id, title, credit, tpc) values ("CSE 482", "Web Development", 3, 1500), ("CSE 418", "Computer Graphics", 3, 1500), ("EEE 311", "Analog Electronics II", 3, 1500), ("PHY 108", "Physics II", 3, 1500), ("PHY 107", "Physics I", 3, 1500), ("CSE 323", "Operating System", 3, 1500), ("CSE 338", "Networking", 3, 1500), ("CSE 231", "Digital Logic Design", 3, 1500), ("CSE 332", "Computer Organization and Architecture", 3, 1500), ("EEE 111", "Analog Electronics I", 3, 1500), ("MAT 120", "Calculus and Analytic Geometry-I", 3, 1500), ("MAT 130", "Calculus and Analytic Geometry-II", 3, 1500), ("MAT 240", "Calculus and Analytic Geometry-III", 3, 1500), ("MAT 250", "Calculus and Analytic Geometry-IV", 3, 1500), ("MAT 350", "Engineering Mathematics", 3, 1500), ("MAT 361", "Probability && Statistics", 3, 1500), ("CSE 215", "Fundamentals of Computer Programming", 3, 1500), ("CSE 115", "Computing Concepts", 3, 1500), ("CSE 225", "Data Structures", 3, 1500), ("EEE 141", "Electrical Circuits", 3, 1500), ("CSE 311", "Database Management System", 3, 1500), ("CSE 273", "Introduction to Theory of Computation", 3, 1500), ("CSE 173", "Discrete Mathematics", 3, 1500);
+            */
         private List<Course> courseList;
         private IExtraFeeCalculator iefc;
         private IDiscountStrategy discountStrategy;
